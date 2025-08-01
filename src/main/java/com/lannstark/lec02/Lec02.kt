4c6743a7ab4a29b1161f42603e92b406ccc2b5f2
devilyn0216 <devilyn0216@gmail.com>
2025-08-01 22:47:04 +0900

    2강. 코틀린에서 null을 다루는 방법

--- src/main/java/com/lannstark/lec02/Lec02.kt
+++ src/main/java/com/lannstark/lec02/Lec02.kt
@@ -1,4 +1,36 @@
 package com.lannstark.lec02
 
-class Lec02 {
+fun main() {
+    val str: String? = "ABC"
+    println(str?.length)
+    println(str?.length ?: 0)
+}
+
+fun startsWithA1(str: String?): Boolean {
+//    if (str == null) {
+//        throw IllegalArgumentException("null 이 들어 왔습니다.")
+//    }
+//    return str.startsWith("A")
+    return str?.startsWith("A") 
+        ?: throw IllegalArgumentException("null 이 들어 왔습니다.")
+}
+
+fun startsWithA2(str: String?): Boolean? {
+//    if (str == null) {
+//        return null
+//    }
+//    return str.startsWith("A")
+    return str?.startsWith("A")
+}
+
+fun startsWithA3(str: String?): Boolean {
+//    if (str == null) {
+//        return false
+//    }
+//    return str.startsWith("A")
+    return str?.startsWith("A") ?: false
+}
+
+fun startsWith(str: String?): Boolean {
+    return str!!.startsWith("A")
 }
--- src/main/java/com/lannstark/lec02/Lec02Main.kt
+++ src/main/java/com/lannstark/lec02/Lec02Main.kt
@@ -1,4 +1,10 @@
 package com.lannstark.lec02
 
-class Lec02Main {
+fun main() {
+    val person = Person("김기태")
+    startsWithA(person.name)
+}
+
+fun startsWithA(str: String): Boolean {
+    return str.startsWith("A")
 }
