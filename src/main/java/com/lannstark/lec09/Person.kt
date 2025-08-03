3c936900499297ce2e33a3b9c24f62df97281946
devilyn0216 <devilyn0216@gmail.com>
2025-08-03 21:44:23 +0900

    9강. 코틀린에서 클래스를 다루는 방법

---
+++ src/main/java/com/lannstark/lec09/Person.kt
@@ -0,0 +1,32 @@
+package com.lannstark.lec09
+
+fun main() {
+    val person = Person("kim", 100)
+    
+    println(person.name)
+    person.age = 33
+    println(person.age)
+}
+
+class Person(
+    val name: String,
+    var age: Int,
+) {
+    init {
+        if (age <= 0) {
+            throw IllegalArgumentException("나이는 ${age}일 수 없습니다")
+        }
+    }
+    
+    constructor(name: String): this(name, 1)
+
+    fun isAdult(): Boolean {
+        return age >= 20
+    }
+    
+    val isAdult: Boolean
+        get() = age >= 20
+    
+    val uppercaseName: String
+        get() = this.name.uppercase()
+}
